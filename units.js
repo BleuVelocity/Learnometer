38b816c37c2a6a276004453d4cdd12594f1db1c5
beanz-on-toast <162033763+beanz-on-toast@users.noreply.github.com>
2024-07-08 15:08:03 +0100

    Update units.js

--- units.js
+++ units.js
@@ -1,5 +1,7 @@
 function convert(){
     var userInput1 = Number(document.getElementById("inputBox").value);
+    var operation = document.getElementById("operation").value;
+    
 
 function goHome() {
     window.location.href = "index.html";
