909e8a860eade04d9093dc3d6dd14ed4aec3d130
beanz-on-toast <162033763+beanz-on-toast@users.noreply.github.com>
2024-07-10 18:35:31 +0100

    Update tests.js

--- tests.js
+++ tests.js
@@ -2,4 +2,6 @@ function print_User_Search(){
   var userSearch = document.getElementById("search_bar").value;
   alert("You searched for: "+userSearch);
 }
-  
+
+function goHome(){
+  window.setScreen(
