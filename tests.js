bbe4da63d617ccbdb277f16c641b06c0dfe9d1ce
beanz-on-toast <162033763+beanz-on-toast@users.noreply.github.com>
2024-07-11 11:13:40 +0100

    Update tests.js

--- tests.js
+++ tests.js
@@ -1,7 +1,6 @@
 function print_User_Search(){
-    //var userSearch = document.getElementById("search_bar").value;
-    //alert("You searched for: "+userSearch);
-    alert("Hi");
+    var userSearch = document.getElementById("search_bar").value;
+    alert("You searched for: "+userSearch);
 }
 
 function goHome() {
